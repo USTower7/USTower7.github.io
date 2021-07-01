2a465da2b426850e84743e7342b57d04a6ecd727
USTower7 <78798982+USTower7@users.noreply.github.com>
2021-07-01 18:21:21 +0400

    a

---
+++ site_8/js/chunk-2d0e923c.787133f9.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e923c"],{"8bea":function(e,n,c){"use strict";c.r(n);var i=c("7a23"),s={class:"mobile"},t=Object(i["g"])("h1",null,"This is mobile",-1);function o(e,n){return Object(i["p"])(),Object(i["d"])("div",s,[t])}const b={};b.render=o;n["default"]=b}}]);
+//# sourceMappingURL=chunk-2d0e923c.787133f9.js.map
